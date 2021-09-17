9b1583f436a5cd3d668ab9650492d6217b631f39
wdingsoft <wdingbox@gmail.com>
2021-09-17 10:53:35 -0400

    ini test ok

---
+++ htm/tester_src.json.js
@@ -0,0 +1,30 @@
+////C:\2016-0819\b\svn\tool\test\audio_player\player
+////C:\2016-0819\b\___bigdata\___biblestudy\ciu\courses\Bib5132-Gospels
+//dir1:{file1:{tm1:info1,tm2:info2,,,},file2:{t1:n1,,}},dir2:{f2:{t2:des2}},,,,
+//https://wdingsoft.github.io/aubiZondervan_OT_FiveMoses/audio/01_Genesis_001.m4a
+var mPlayerResources={
+
+    path_local_old:"https://wdingsoft.github.io/aubiZondervan_OT_FiveMoses/audio/01_Genesis_001.m4a",
+    path:"https://wdingsoft.github.io/aubiZondervan_OT_FiveMoses/audio/",
+    files:{
+    "01_Genesis_001.m4a":{"00:00:00.0":"、","00:53:20.0":"observation1"},
+    "01_Genesis_002.m4a":{"00:00:00.0":"、","01:53:20.0":"observation1"},
+    
+    
+    
+    
+    },/////////////////
+    
+    
+    
+    
+    
+    
+    
+    
+    };///////////////////////////////////////////////////////////
+    
+    /**************
+    {"filename":{"starttime":"des","":""}}
+    
+    ***************/
